c0b6a22900b269282a950fca77631378fbb60dd7
javadev111 <43318339+javadev111@users.noreply.github.com>
2018-12-25 12:09:18 +0300

    Дз к 3му уроку с hard

--- lesson3.py
+++ lesson3.py
@@ -53,6 +53,7 @@ def fibonacci(n, m):
             fibo.append(next_fibo)
     return fibo[n - 1: m]
 
+
 print(fibonacci(4, 20))
 
 
@@ -84,7 +85,7 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 def user_filter(function_object, iterable):
     f_iterable = []
     for i in iterable:
-        if function_object() == True:
+        if function_object():
             f_iterable.append(i)
     return f_iterable
 
@@ -108,3 +109,78 @@ if calc_coord(A1, A3) == calc_coord(A2, A4):
     print('Это могут быть вершины параллелограмма.')
 else:
     print('Это не вершины параллелограмма.')
+
+
+# hard
+# Задание-2:
+# Дана ведомость расчета заработной платы (файл "data/workers").
+# Рассчитайте зарплату всех работников, зная что они получат полный оклад,
+# если отработают норму часов. Если же они отработали меньше нормы,
+# то их ЗП уменьшается пропорционально, а за заждый час переработки
+# они получают удвоенную ЗП, пропорциональную норме.
+# Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
+
+def read_file(f):
+    f_copy = f.read()
+    while '  ' in f_copy:
+        f_copy = f_copy.replace('  ', ' ')
+    f_copy = f_copy.split('\n')
+    read_list = []
+    for i in f_copy:
+        read_list.append(i.split(' '))
+    return read_list
+
+
+with open('data/workers.txt', 'r', encoding='utf-8') as f:
+    workers_list = read_file(f)
+
+
+with open("data/hours_of.txt", 'r', encoding='utf-8') as f1:
+    hourse_of_list = read_file(f1)
+
+
+wage_list = []
+for i in range(1, len(workers_list)):
+
+    salary = int(workers_list[i][2])
+    hour_rate = int(workers_list[i][4])
+
+    for j in range(1, len(hourse_of_list)):
+
+        hourse_of = int(hourse_of_list[j][2])
+
+        if hourse_of_list[j][0] == workers_list[i][0] and hourse_of_list[j][1] == workers_list[i][1]:
+
+            wage = salary / hour_rate * hourse_of
+            if hourse_of > hour_rate:
+                wage = salary +  2 * salary / hour_rate * (hourse_of - hour_rate)
+
+            wage_list.append([workers_list[i][0], workers_list[i][1], wage])
+            break
+
+print(wage_list)
+
+# Задание-3:
+# Дан файл ("data/fruits") со списком фруктов.
+# Записать в новые файлы все фрукты, начинающиеся с определенной буквы.
+# Т.е. в одном файле будут все фрукты на букву “А”, во втором на “Б” и т.д.
+# Файлы назвать соответственно.
+# Пример имен файлов: fruits_А, fruits_Б, fruits_В ….
+# Важно! Обратите внимание, что нет фруктов, начинающихся с некоторых букв.
+# Напишите универсальный код, который будет работать с любым списком фруктов
+# и распределять по файлам в зависимости от первых букв, имеющихся в списке фруктов.
+# Подсказка:
+# Чтобы получить список больших букв русского алфавита:
+# print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+
+with open("data/fruits.txt", 'r', encoding='utf-8') as f:
+    f_copy = (f.read().split('\n\n'))
+
+rus_alphabet_list = list(map(chr, range(ord('А'), ord('Я')+1)))
+for i in rus_alphabet_list:
+    for j in f_copy:
+        if j[0] == i:
+            file = open(f'data/fruits_{i}.txt', 'a', encoding='utf-8')
+            file.write(f'{j}\n\n')
+            file.close()
